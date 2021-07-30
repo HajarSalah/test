4d12473aa5bff87ac9e3a3014463358d5b3b4e55
HajarSalah <87914336+HajarSalah@users.noreply.github.com>
2021-07-30 14:29:23 +0300

    "Added Hajoora Salooha"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello There");
+	System.out.println("Hajoora Salooha");
         Nawa.pr();
     }
 }
